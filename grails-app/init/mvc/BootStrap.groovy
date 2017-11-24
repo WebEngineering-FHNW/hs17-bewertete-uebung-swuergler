034d612f22a6e522f3a5f4f149cbb4d73d624942
Samuel Würgler <samuel.wuergler@students.fhnw.ch>
2017-11-24 23:55:36 +0100

    Cleaned up the project

--- grails-app/init/mvc/BootStrap.groovy
+++ grails-app/init/mvc/BootStrap.groovy
@@ -9,22 +9,10 @@ class BootStrap {
             return
         }
 
-        Room windowRoom = save(new Room(name: "1.333", max: 32))
-        Room cornerRoom = save(new Room(name: "1.331", max: 42))
-
-        Person dierk = save(new Person(firstName: "Dierk", lastName: "König", email:"dierk.koenig@fhnw.ch"))
-
-
         Squad test  = save(new Squad(squadname: "Test"))
         User admin = save (new User(squad: test, username: "admin", password: "123", adminUser: true))
-        Task testTask = save(new Task(taskTitle: "empty bin", taskDescription: "empty all bins in office", done: false, assignee: admin))
-        Task testTask2 = save(new Task(taskTitle: "clean room", taskDescription: "clean office room", done: false, assignee: admin))
-
-        Date today = new Date().clearTime();
-        save(new Booking(booker: dierk, room: windowRoom, date: today - 1, slot: Booking.AM))
-        save(new Booking(booker: dierk, room: windowRoom, date: today,     slot: Booking.AM))
-        save(new Booking(booker: dierk, room: windowRoom, date: today + 1, slot: Booking.AM))
-        save(new Booking(booker: dierk, room: cornerRoom, date: today ,    slot: Booking.AM))
+        save(new Task(taskTitle: "empty bin", taskDescription: "empty all bins in office", done: false, assignee: admin))
+        save(new Task(taskTitle: "clean room", taskDescription: "clean office room", done: false, assignee: admin))
 
     }
 
